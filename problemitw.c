7c6cfd45d7e5f742c3e649e7e993bfcb2c32b1a9
bhuvaneshmakka <bt23csh019@iiitn.ac.in>
2024-11-14 17:04:59 +0530

    Add files via upload

---
+++ problemitw.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a;
+    printf("Enter a integer \n");
+    scanf("%d",&a);
+    printf("Your selected integer is %d \n",a);
+    char b;
+    printf("Enter a character \n");
+    scanf(" %c",&b);
+    printf("Your selected character is %c\n",b);
+}
